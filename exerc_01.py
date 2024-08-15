e08283f09d2e9259bcce0c1febe524995bb7432b
PedroS <pedrovsg94@gmail.com>
2024-08-15 01:33:03 -0300

    questão 01

---
+++ exerc_01.py
@@ -0,0 +1,6 @@
+#1) Escreva um programa para ler o raio de um círculo, 
+# calcular e escrever a sua área (Pi * (raio * raio)):
+
+raio = 12
+Pi = 3.14
+print('O valor da aréa é de:', Pi * (raio*raio))
